c3780d769dcd2c67bac84a2e5a33a193c952e33d
zhanghuajun <zhanghuajun@ntces.com>
2019-01-06 00:11:53 +0800

    increase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/dashboard/Dashboard'
+import Dashboard from './components/dashboard/Dashboard';
+import Banners from './components/ui/banners';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Dashboard />
+         {/* <Dashboard /> */}
+         <Banners />
       </div>
     );
   }
